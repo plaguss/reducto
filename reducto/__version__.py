762e1fbda41483738629a0ef2f240e673a6f0983
Agustin <agustin.piqueres@gmail.com>
2021-02-28 11:18:37 +0100

    feat: version file added

---
+++ reducto/__version__.py
@@ -0,0 +1,11 @@
+
+VERSION = (0, 1, 0)
+
+__version__ = '.'.join(map(str, VERSION))
+__title__ = 'reducto'
+__description__ = 'Reduce a python package to basic readability features.'
+#__url__ = ''
+#__build__ = 0x022501
+__author__ = 'Agustin Piqueres'
+__author_email__ = 'agustin.piqueres@gmail.com'
+__license__ = 'MIT'
