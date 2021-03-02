4d7774a031e8b3e111ffef5363be00ed25583344
Agustin <agustin.piqueres@gmail.com>
2021-03-02 21:22:17 +0100

    feat: pyfile extension addition

---
+++ reducto/_ext/pyfile_wrap.pyx
@@ -0,0 +1,72 @@
+# distutils: language=c++
+# cython: c_string_type=unicode, c_string_encoding=utf8
+# cython: language_level=3
+
+
+from cython.operator cimport dereference as deref
+from libcpp.string cimport string
+from libcpp cimport bool
+
+
+cdef extern from "_pyfile.h" namespace "Reducto":
+
+    cdef cppclass PyFileCpp:
+        PyFileCpp() except +
+        PyFileCpp(const string&) except +
+        void collect()
+        int number_of_functions()
+        int number_of_lines()
+        int number_of_comment_lines()
+        int number_of_blank_lines()
+        int number_of_docstring_lines()
+        int get_max_line_length()
+        void get_functions()
+        # void grab_functions_info()
+        # void print_functions_info()
+
+        # FileStats stats();  # TODO: Y este?
+
+
+cdef class PyFile:
+    cdef PyFileCpp* _thisptr
+
+    def __cinit__(self, const string& l):
+        self._thisptr = new PyFileCpp(l)
+
+    def __dealloc__(self):
+        if self._thisptr != NULL:
+            del self._thisptr
+
+    property name:
+        def __get__(self):
+            return deref(self._thisptr).filename
+
+        def __set__(self, _name):
+            deref(self._thisptr).filename = _name
+
+    def collect(self):
+        return self._thisptr.collect()
+
+    def number_of_functions(self):
+        return self._thisptr.number_of_functions()
+
+    def number_of_lines(self):
+        return self._thisptr.number_of_lines()
+
+    def number_of_comment_lines(self):
+        return self._thisptr.number_of_comment_lines()
+
+    def number_of_blank_lines(self):
+        return self._thisptr.number_of_blank_lines()
+
+    def number_of_docstring_lines(self):
+        return self._thisptr.number_of_docstring_lines()
+
+    def get_max_line_length(self):
+        return self._thisptr.get_max_line_length()
+
+    def stats(self):
+        # TODO: Ayuda con FileStats
+        # Tendría que ser un cpdef para devolver Python/Cpp!
+        # https://groups.google.com/g/cython-users/c/39Nwqsksdto
+        pass
