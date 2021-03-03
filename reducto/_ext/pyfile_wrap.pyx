10e4458ae0ec0859bab5b0a3e130ef8a6ff1d806
Agustin <agustin.piqueres@gmail.com>
2021-03-03 23:20:46 +0100

    feat: definition of pyfile wrap

--- reducto/_ext/pyfile_wrap.pyx
+++ reducto/_ext/pyfile_wrap.pyx
@@ -11,7 +11,12 @@ from libcpp.vector cimport vector
 cdef extern from "_pyfile.h" namespace "Reducto":
 
     cdef cppclass FileStats:
-        FileStats() except +
+        int number_of_lines
+        int max_line_length
+        int docstrings
+        int comment_lines
+        int blank_lines
+        vector[int] functions
 
     cdef cppclass PyFileCpp:
         PyFileCpp() except +
@@ -26,28 +31,19 @@ cdef extern from "_pyfile.h" namespace "Reducto":
         void get_functions()
         # void grab_functions_info()
         # void print_functions_info()
-
+        dict stats()
         # FileStats stats();  # TODO: Y este?
-
-# cdef
 
 
 cdef class PyFile:
     cdef PyFileCpp* _thisptr
 
-    def __cinit__(self, const string& l):
-        self._thisptr = new PyFileCpp(l)
+    def __cinit__(self, const string& f):
+        self._thisptr = new PyFileCpp(f)
 
     def __dealloc__(self):
         if self._thisptr != NULL:
             del self._thisptr
-
-    property name:
-        def __get__(self):
-            return deref(self._thisptr).filename
-
-        def __set__(self, _name):
-            deref(self._thisptr).filename = _name
 
     def collect(self):
         return self._thisptr.collect()
@@ -70,9 +66,26 @@ cdef class PyFile:
     def get_max_line_length(self):
         return self._thisptr.get_max_line_length()
 
+    cdef dict _stats(self):
+        FileStats _stats = self._thisptr.stats()
+        cdef dict results
+
+        results = {
+            "number_of_lines": _stats.number_of_lines,
+            "max_line_length": _stats.max_line_length,
+            "docstrings": _stats.docstrings,
+            "comment_lines": _stats.comment_lines,
+            "blank_lines": _stats.blank_lines,
+            "function_lengths": _stats.funtions
+        }
+
+        return results
+
     def stats(self):
         # TODO: Ayuda con FileStats
         # Tendría que ser un cpdef para devolver Python/Cpp!
         # https://groups.google.com/g/cython-users/c/39Nwqsksdto
-        cdef FileStats _stats = self._thisptr.stats()
+        # https://stackoverflow.com/questions/63570432/dectlaration-of-function-that-returns-dictionary-in-cython
+        # https://stackoverflow.com/questions/9781572/c-struct-inheritance-in-cython
+        return self._stats()
         # TODO: acceder a la info internamente y devolver dict
