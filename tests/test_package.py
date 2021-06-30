fb52bc3f0816b18a2312a982cac17b27b1bdf3e1
agustin <agustin.piqueres@gmail.com>
2021-06-30 23:07:41 +0200

    test: test failing on index error

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,7 +5,6 @@ Contains tests related to reducto/package.py
 from typing import List
 import pytest
 import pathlib
-import tempfile
 
 import reducto.package as pkg
 import reducto.src as src
@@ -39,14 +38,24 @@ def test_tmp(sample_package):
     assert sample_package.is_file()
 
 
-def test_is_package():
-    assert 1 == 0
+def test_is_package(sample_package):
+    assert pkg.is_package(sample_package)
+
+
+def test_is_src_package(src_sample_package):
+    assert pkg.is_src_package(src_sample_package)
 
 
 class TestPackage:
-    @pytest.fixture(scope='class')
+    @pytest.fixture
     def package(self, sample_package):
         return pkg.Package(sample_package)
+
+    def test_package_validate(self, sample_package):
+        with pytest.raises(pkg.PackageError):
+            pkg.Package(sample_package / 'data')
+        pkg.Package(sample_package)
+        pkg.Package(sample_package / 'src')
 
     def test_name(self, package, sample_package):
         assert package.name == sample_package.name
@@ -55,29 +64,40 @@ class TestPackage:
         assert repr(package) == f'Package({sample_package.name})'
 
     def test_package_len(self, package):
-        assert len(package) == 6
+
+        assert len(package) == 528
 
     def test_source_files(self, package):
         assert isinstance(package.source_files, list)
         assert all(isinstance(f, src.SourceFile) for f in package.source_files)
+        assert len(package.source_files) == 6
 
     def test_package_lines(self, package):
-        assert package.lines == 30  # Sum of docstring lines from every SourceFile
+        assert isinstance(package.lines, list)
+        assert all(a == b for a, b in zip(package.lines, [0, 128, 1, 128, 128, 128]))
 
     def test_package_docstrings(self, package):
         assert package.docstrings == 30  # Sum of docstring lines from every SourceFile
 
     def test_package_comments(self, package):
-        assert package.comments == 30  # Sum of docstring lines from every SourceFile
+        assert isinstance(package.blank_lines, list)
+        assert all(a == b for a, b in zip(package.comment_lines, [0, 3, 0, 3, 3, 3]))
 
     def test_package_blank_lines(self, package):
-        assert package.blank_lines == 30  # Sum of docstring lines from every SourceFile
+        assert isinstance(package.blank_lines, list)
+        assert all(a == b for a, b in zip(package.blank_lines, [0, 32, 1, 32, 32, 32]))
 
     def test_package_source_lines(self, package):
         assert package.source_lines == 30  # Sum of docstring lines from every SourceFile
 
     def test_package_number_of_functions(self, package):
-        assert package.number_of_functions == 30
+        assert package.functions == 30
 
     def test_package_average_function_length(self, package):
         assert package.average_function_length == 30
+
+    def test_package_walk(self, package):
+        walked = package._walk()
+        print(list(walked))
+        print([str(f) for f in package._walk()])
+        assert 1 == 0
