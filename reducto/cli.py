f8716a875aa679c9f86621b1ee0c5201fea61029
agustin <agustin.piqueres@gmail.com>
2021-07-06 22:47:06 +0200

    Add main command to execute the app

--- reducto/cli.py
+++ reducto/cli.py
@@ -1,3 +1,16 @@
-"""Call the Command Line Interface here.
+"""Call the Command Line Interface here. """
 
-"""
+import sys
+from typing import List
+from typing import Optional
+
+from reducto import reducto
+
+
+def main(argv: Optional[List[str]] = None) -> None:
+    """Execute the reducto application. """
+    if argv is None:
+        argv = sys.argv[1:]
+
+    app = reducto.Reducto()
+    app.run(argv)
