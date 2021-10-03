fadb35f92488c9045cb3ed8964f4f7464fa2fa69
agustin <agustin.piqueres@gmail.com>
2021-10-03 23:30:28 +0200

    fix: corrected package description

--- reducto/__init__.py
+++ reducto/__init__.py
@@ -1,5 +1,5 @@
-"""Reduce a package as a set of basic source code statistics. """
+"""Python source code stats in a command. """
 
 from reducto.cli import main
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
